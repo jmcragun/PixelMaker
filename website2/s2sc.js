f2781983f2aa28cc3c726a8cd72740744e692f4e
Joshua Cragun <jmcragun@gmail.com>
2016-08-02 11:35:51 -0600

    Updated PixelMaker

--- website2/s2sc.js
+++ website2/s2sc.js
@@ -1,6 +1,15 @@
 $(document).ready(function () {
+	for (var i =  0; i < 2500; i++) {
+		$(".container").append("<div class=\"canvas\"></div>");
+	}
 	$(".canvas").click(function () {
 		var color = $("#colorval").val();
 		$(this).css("background-color", color);
+	});
+	$(document).on("dblclick", ".canvas", function () {
+		$(this).css("background-color", "white");
+	});
+	$("#clearAll").click(function () {
+		$(".canvas").css("background-color", "white");
 	});
 });
